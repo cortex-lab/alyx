e5ff396e6a065063aac5646ec924735b9202902e
Max Hunter <nippoo@nippoo.net>
2016-01-14 15:17:38 +0000

    Virus injections

--- subjects/models.py
+++ subjects/models.py
@@ -4,6 +4,10 @@ from django.contrib.auth.models import User
 from django.contrib.postgres.fields import ArrayField, JSONField
 from datetime import datetime
 
+###############################################################################
+### Subjects
+###############################################################################
+
 class Species(models.Model):
     binomial = models.CharField(max_length=255, primary_key=True)
     display_name = models.CharField(max_length=255)
@@ -45,8 +49,14 @@ class Subject(models.Model):
 
 class Litter(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    descriptive_name = models.CharField(max_length=255)
     mother = models.ForeignKey("Subject", null=True, blank=True, related_name="litter_mother")
     father = models.ForeignKey("Subject", null=True, blank=True, related_name="litter_father")
+
+
+###############################################################################
+### Actions
+###############################################################################
 
 class Action(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -60,6 +70,27 @@ class Action(models.Model):
                       null=True,
                       blank=True)
     JSON = JSONField(null=True, blank=True)
+
+
+class Virus_Batch(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    virus_type =  models.CharField(max_length=255, null=True, blank=True) # upenn ID or equivalent
+    description = models.CharField(max_length=255, null=True, blank=True)
+    virus_source = models.CharField(max_length=255, null=True, blank=True) # who sold it
+    date_time_made = models.DateTimeField(null=True, blank=True, default=datetime.now)
+    nominal_titer = models.FloatField(null=True, blank=True)
+
+class Virus_Injection(Action):
+    INJECTION_TYPES = (
+        ('I', 'Iontophoresis'),
+        ('P', 'Pressure'),
+    )
+    virus_batch_id = models.ForeignKey('Virus_Batch') # links to virus_batch document, defined below
+    injection_volume = models.FloatField(null=True, blank=True)
+    rate_of_injection = models.FloatField(null=True, blank=True)
+    injection_type = models.CharField(max_length=1,
+                                     choices=INJECTION_TYPES,
+                                     default='I', blank=True, null=True)
 
 class Weighing(Action):
     weight = models.FloatField()
