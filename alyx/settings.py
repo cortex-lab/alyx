a8910922968d38756f3e905a18946d66cd33d254
Max Hunter <nippoo@nippoo.net>
2016-04-11 17:59:29 +0100

    Removed production secrets and refactored into settings_secret

--- alyx/settings.py
+++ alyx/settings.py
@@ -12,15 +12,13 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from .settings_secret import *
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'yj6u+*w*er&*ym32k33x6p6v$v+3_pmrf%e3eg-_9+0%2lu-1)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -71,22 +69,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'alyx.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'labdb',
-        'USER': 'labdb',
-        'PASSWORD': 'dbdbdb',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
