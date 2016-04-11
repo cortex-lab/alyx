63a2d856e8dfe48b8c656636f8a3d8b7f938fb8a
Max Hunter <nippoo@nippoo.net>
2016-04-11 17:56:12 +0100

    Renamed last traces of labdb to alyx

--- alyx/urls.py
+++ alyx/urls.py
@@ -1,4 +1,4 @@
-"""labdb URL Configuration
+"""alyx URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
--- alyx/wsgi.py
+++ alyx/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for labdb project.
+WSGI config for alyx project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "labdb.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alyx.settings")
 
 application = get_wsgi_application()
