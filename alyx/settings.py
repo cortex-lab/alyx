21b53aa5da4afeb60dc8f2478459e9b14357684b
Max Hunter <nippoo@nippoo.net>
2016-04-16 21:35:08 +0100

    Added rest_framework_swagger

--- alyx/settings.py
+++ alyx/settings.py
@@ -92,9 +92,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
