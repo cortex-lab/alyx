e0f1cfdbea15fe784d5719a101bdde44cc5eceba
Max Hunter <nippoo@nippoo.net>
2016-07-06 19:43:40 +0100

    Removed githubpages from sphinx extensions (was failing build)

--- docs/conf.py
+++ docs/conf.py
@@ -98,7 +98,6 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
