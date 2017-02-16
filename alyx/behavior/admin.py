c720bdead971a0ccb22d00d87b885c230eedd254
Cyrille Rossant <cyrille.rossant@gmail.com>
2017-02-16 08:36:22 +0000

    Tweak behavior admin

--- alyx/behavior/admin.py
+++ alyx/behavior/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import *
+from alyx.base import BaseAdmin
 
-admin.site.register(PupilTracking)
+
+class PupilTrackingAdmin(BaseAdmin):
+    fields = ['description', 'movie', 'eye', 'x_y_d',
+              'generating_software', 'provenance_directory']
+
+
+admin.site.register(PupilTracking, PupilTrackingAdmin)
