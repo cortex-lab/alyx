214ed3f31029d9279191eb8f2619f70f6dab5867
nsteinme <nick.steinmetz@gmail.com>
2017-02-26 15:29:43 +0000

    first version of some SQL queries for generating backups

---
+++ alyx/backups/queries/subjects_subject.sql
@@ -0,0 +1,30 @@
+SELECT subjects_subject.id,
+subjects_subject.nickname,
+subjects_subject.sex,
+subjects_subject.birth_date,
+subjects_subject.death_date,
+subjects_subject.implant_weight,
+subjects_subject.notes,
+subjects_subject.ear_mark,
+subjects_cage.cage_label AS cage,
+subjects_line.name AS line,
+subjects_litter.descriptive_name AS litter,
+auth_user.username AS responsible_user,
+subjects_source.name AS source,
+subjects_species.display_name AS species,
+subjects_strain.descriptive_name AS strain,
+subjects_subject.wean_date,
+subjects_subject.actual_severity,
+subjects_subject.adverse_effects,
+subjects_subject.cull_method,
+subjects_subject.json,
+subjects_subject.request_id
+FROM subjects_subject
+LEFT JOIN subjects_line on subjects_subject.line_id=subjects_line.id
+LEFT JOIN subjects_cage on subjects_subject.cage_id=subjects_cage.id
+LEFT JOIN auth_user on subjects_subject.responsible_user_id=auth_user.id
+LEFT JOIN subjects_litter on subjects_subject.litter_id=subjects_litter.id
+LEFT JOIN subjects_source on subjects_subject.source_id=subjects_source.id
+LEFT JOIN subjects_species on subjects_subject.species_id=subjects_species.id
+LEFT JOIN subjects_strain on subjects_subject.strain_id=subjects_strain.id
+ORDER BY subjects_subject.birth_date DESC
---
+++ alyx/backups/queries/subjects_subjectrequest.sql
@@ -0,0 +1,11 @@
+SELECT subjects_subjectrequest.id,
+subjects_subjectrequest.json,
+subjects_subjectrequest.count,
+subjects_subjectrequest.date_time,
+subjects_subjectrequest.due_date,
+subjects_subjectrequest.notes,
+subjects_line.name AS line_name,
+auth_user.username AS username
+FROM subjects_subjectrequest 
+INNER JOIN subjects_line on subjects_subjectrequest.line_id=subjects_line.id
+INNER JOIN auth_user on subjects_subjectrequest.user_id=auth_user.id
