85ab3426b47344e894e549620d0bcd355f497660
Cyrille Rossant <cyrille.rossant@gmail.com>
2017-02-27 14:49:13 +0100

    WIP

--- alyx/backups/backup-tsv.sh
+++ alyx/backups/backup-tsv.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 output_dir='tsv_files'
 mkdir -p $output_dir
+host='rod.cortexlab.net'
+user='alyx_ro'
+port=5432
+database='alyx'
 for name in queries/*.sql; do
     bn=$(basename $name)
     bn_noext=${bn%.*}
-    psql -h localhost -U cyrille -p 5432 -d labdb -c "\copy ($(cat $name)) to '$output_dir/$bn_noext.tsv' with CSV DELIMITER E'\t' header encoding 'utf-8'"
+    psql -h $host -U $user -p $port -d $database -c "\copy ($(cat $name)) to '$output_dir/$bn_noext.tsv' with CSV DELIMITER E'\t' header encoding 'utf-8'"
 done
