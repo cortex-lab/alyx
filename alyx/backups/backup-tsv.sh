a5737ce73f5b79865bf60f4373be31761944884c
Cyrille Rossant <cyrille.rossant@gmail.com>
2017-02-28 16:22:52 +0100

    Can specify root dir for backup as script argument

--- alyx/backups/backup-tsv.sh
+++ alyx/backups/backup-tsv.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
-output_dir='tsv_files'
+today=`date +%Y-%m-%d`
+root_dir=$1
+if [ -z "$1" ]; then
+    root_dir='.'
+fi
+output_dir="$root_dir/alyx-backups/$today/"
 mkdir -p $output_dir
 host='rod.cortexlab.net'
 user='alyx_ro'
-port=5432
 database='alyx'
+port=5432
 for name in queries/*.sql; do
     bn=$(basename $name)
     bn_noext=${bn%.*}
     psql -h $host -U $user -p $port -d $database -c "\copy ($(cat $name)) to '$output_dir/$bn_noext.tsv' with CSV DELIMITER E'\t' header encoding 'utf-8'"
 done
+echo "Backup done in $output_dir"
