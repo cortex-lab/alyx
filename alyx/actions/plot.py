3729088be361e9a709515424cccb55ce4cc84fba
Cyrille Rossant <cyrille.rossant@gmail.com>
2017-03-02 17:14:55 +0100

    Put back weighing plot

--- alyx/actions/plot.py
+++ alyx/actions/plot.py
@@ -1,9 +1,4 @@
 import io
-# import matplotlib
-# matplotlib.use('Agg')  # noqa
-# import matplotlib.pyplot as plt
-# from matplotlib.dates import DayLocator, DateFormatter
-# import seaborn  # noqa
 
 from django.http import HttpResponse
 from subjects.models import Subject
@@ -11,26 +6,33 @@ from actions.models import Weighing
 
 
 def weighing_plot(request, subject_id=None):
-    return HttpResponse(None)
-    # if subject_id in (None, 'None'):
-    #     return HttpResponse('')
-    # subj = Subject.objects.get(pk=subject_id)
-    # weighins = Weighing.objects.filter(
-    #     subject_id=subj.id).order_by('date_time')
-    # if not weighins:
-    #     return HttpResponse('')
-    # x, y = zip(*((w.date_time, w.weight) for w in weighins))
+    if not request.user.is_authenticated():
+        return HttpResponse('')
 
-    # f, ax = plt.subplots(1, 1, figsize=(8, 2))
-    # ax.xaxis.set_major_locator(DayLocator())
-    # ax.xaxis.set_major_formatter(DateFormatter('%d/%m/%y'))
+    import matplotlib
+    matplotlib.use('Agg')
+    import matplotlib.pyplot as plt
+    from matplotlib.dates import DayLocator, DateFormatter
+    # plt.style.use('seaborn')
 
-    # ax.plot(x, y)
-    # ax.set_title("Weighings for %s" % subj.nickname)
-    # plt.tight_layout()
-    # plt.grid('on')
+    if subject_id in (None, 'None'):
+        return HttpResponse('')
+    subj = Subject.objects.get(pk=subject_id)
+    weighins = Weighing.objects.filter(
+        subject_id=subj.id).order_by('date_time')
 
-    # buf = io.BytesIO()
-    # f.savefig(buf, format='png')
-    # buf.seek(0)
-    # return HttpResponse(buf.read(), content_type="image/png")
+    f, ax = plt.subplots(1, 1, figsize=(8, 2))
+    ax.xaxis.set_major_locator(DayLocator())
+    ax.xaxis.set_major_formatter(DateFormatter('%d/%m/%y'))
+
+    if weighins:
+        x, y = zip(*((w.date_time, w.weight) for w in weighins))
+        ax.plot(x, y)
+    ax.set_title("Weighings for %s" % subj.nickname)
+    plt.tight_layout()
+    plt.grid('on')
+
+    buf = io.BytesIO()
+    f.savefig(buf, format='png')
+    buf.seek(0)
+    return HttpResponse(buf.read(), content_type="image/png")
