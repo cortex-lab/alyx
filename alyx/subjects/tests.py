a08b2715c7ab0288775516384a8a17216d9a6914
Cyrille Rossant <cyrille.rossant@gmail.com>
2017-03-03 17:02:23 +0100

    WIP: tests

--- alyx/subjects/tests.py
+++ alyx/subjects/tests.py
@@ -2,8 +2,8 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test.client import RequestFactory
 
-from .admin import MyAdminSite, SubjectAdmin
-from .models import Subject
+from .admin import *
+from .models import *
 
 
 class MockSuperUser(User):
@@ -26,17 +26,29 @@ class ModelAdminTests(TestCase):
     def ar(self, r):
         assert r.status_code == 200
 
-    def test_subjects_1(self):
-        ma = SubjectAdmin(Subject, self.site)
-
+    def _test_list_change(self, ma):
         # List of subjects.
         r = ma.changelist_view(self.request)
         self.ar(r)
 
         # Get the first subject.
         qs = ma.get_queryset(self.request)
+        if not len(qs):
+            return
         subj = qs[0]
 
         # Change page.
         r = ma.change_view(self.request, subj.id.hex)
         self.ar(r)
+
+    def test_model_admins(self):
+        self._test_list_change(SubjectAdmin(Subject, self.site))
+        self._test_list_change(CageAdmin(Cage, self.site))
+        self._test_list_change(LitterAdmin(Litter, self.site))
+        self._test_list_change(LineAdmin(Line, self.site))
+        self._test_list_change(SpeciesAdmin(Species, self.site))
+        self._test_list_change(StrainAdmin(Strain, self.site))
+        self._test_list_change(AlleleAdmin(Allele, self.site))
+        self._test_list_change(SourceAdmin(Source, self.site))
+        self._test_list_change(SequenceAdmin(Sequence, self.site))
+        self._test_list_change(SubjectAdverseEffectsAdmin(Subject, self.site))
