d1a4ba71dbdf68131a858bff12ad4e1f832b58ed
Cyrille Rossant <cyrille.rossant@gmail.com>
2017-03-10 09:53:13 +0100

    Add zygosities

--- alyx/subjects/zygosities.py
+++ alyx/subjects/zygosities.py
@@ -15,4 +15,98 @@ ZYGOSITY_RULES = (
 
     ('TetO.CtTa', 'Camk2a-tTa', (('tTA', '+/-'),
                                  ('-tTA', '-/-'))),
+
+    ('N.KHA', 'Ntsr1-Cre', (('Cre', '+/-'),  # need to check on this one too.
+                            ('-Cre', '-/-'))),
+
+    ('Thy18.Pv', 'Thy18-ChR2', (('YFP', '+'),
+                                ('-YFP', '-/-'))),
+
+    ('RCL.Vg', 'Ai95-G6f', (('GFP', '+/-'),
+                            ('-GFP', '-/-'))),
+
+    ('RCL.Vg', 'Vglut1-Cre', (('Cre', '+/-'),
+                              ('-Cre', '-/-'))),
+
+    ('Ai78.CtTa', 'Camk2a-tTa', (('tTA', '+/-'),
+                                 ('-tTA', '-/-'))),
+
+    ('Ai78.CtTa', 'Ai78-VSFP', (('VSFP', '+/-'),  # I think I need to ask about this one...
+                                ('-VSFP', '-/-'))),
+
+    ('Vip', 'Vip-Cre', (('Cre,Vip WT', '+/-'),
+                        ('Cre,-Vip WT', '+/+'),
+                        ('-Cre', '-/-'))),
+
+    ('Vg', 'Vglut1-Cre', (('Cre,WT', '+/-'),
+                          ('Cre,-WT', '+/+'),
+                          ('-Cre', '-/-'))),
+
+    ('Thy18', 'Thy18-ChR2', (('YFP', '+'),
+                             ('-YFP', '-/-'))),
+
+    ('TdTom', 'TdTom-RFP', (('tdRFP,ROSA WT', '+/-'),
+                            ('tdRFP,-ROSA WT', '+/+'),
+                            ('-tdRFP', '-/-'))),
+
+    ('Sst', 'Sst-Cre', (('Cre,Sst WT', '+/-'),
+                        ('Cre,-Sst WT', '+/+'),
+                        ('-Cre', '-/-'))),
+
+    # note I think spreadsheet has a typo here, currently has Cre and Sst WT,
+    # but I think it should be these
+    ('Snap', 'Snap25-G6s', (('GFP,WT', '+/-'),
+                            ('GFP,-WT', '+/+'),
+                            ('-GFP', '-/-'))),
+
+    ('RCL', 'Ai95-G6f', (('eGFP,WT', '+/-'),
+                         ('eGFP,-WT', '+/+'),
+                         ('-eGFP', '-/-'))),
+
+    ('Rasgrf', 'Rasgrf-Cre', (('Rasgrf Cre,Rasgrf WT', '+/-'),
+                              ('Rasgrf Cre,-Rasgrf WT', '+/+'),
+                              ('-Rasgrf Cre', '-/-'))),
+
+    #  fwiw it is intentional that the logic is inverted here relative to usual
+    ('KHA', 'KHA1-KO', (('Vglut1 MD,Vglut1 WT', '+/-'),
+                        ('Vglut1 MD,-Vglut1 WT', '-/-'),
+                        ('-Vglut1 MD', '+/+'))),
+
+    ('Gad', 'Gad-Cre', (('Cre,WT', '+/-'),
+                        ('Cre,-WT', '+/+'),
+                        ('-Cre', '-/-'))),
+
+    ('CtTa', 'Camk2a-tTa', (('tTA', '+'),
+                            ('-tTA', '-/-'))),
+
+    ('Pv', 'Pv-Cre', (('Cre,WT', '+/-'),
+                      ('Cre,-WT', '+/+'),
+                      ('-Cre', '-/-'))),
+
+    ('TetO', 'TetO-G6s', (('TRE,WT', '+/-'),
+                          ('TRE,-WT', '+/+'),
+                          ('-TRE', '-/-'))),
+
+    ('TetO.CtTa', 'TetO-G6s', (('TRE', '+/-'),
+                               ('-TRE', '-/-'))),
+
+    ('TetO.CtTa', 'Camk2a-tTa', (('tTA', '+/-'),
+                                 ('-tTA', '-/-'))),
+
+    ('Ai32', 'Ai32-ChR2', (('ChR2-YFP,WT', '+/-'),
+                           ('ChR2-YFP,-WT', '+/+'),
+                           ('-ChR2-YFP', '-/-'))),
+
+    ('DA', 'DAT-Cre', (('Cre,WT', '+/-'),
+                       ('Cre,-WT', '+/+'),
+                       ('-Cre', '-/-'))),
+
+    ('Drd', 'Drd1a-Cre', (('Cre,WT', '+/-'),
+                          ('Cre,-WT', '+/+'),
+                          ('-Cre', '-/-'))),
+
+    ('Emx', 'Emx1-Cre', (('Cre,WT', '+/-'),
+                         ('Cre,-WT', '+/+'),
+                         ('-Cre', '-/-'))),
+
 )
