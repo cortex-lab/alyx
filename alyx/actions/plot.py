efdf8db06dbfee673cc4ffe17d364f9a7261552a
Cyrille Rossant <cyrille.rossant@gmail.com>
2017-03-16 13:41:32 +0100

    WIP: improve plot

--- alyx/actions/plot.py
+++ alyx/actions/plot.py
@@ -5,37 +5,56 @@ from subjects.models import Subject
 from actions.models import Weighing
 
 
+def _plot_weighings(ax, weighings, coeff=1., *args, **kwargs):
+    if not weighings:
+        return
+    x, y = zip(*((w.date_time, w.weight * coeff) for w in weighings))
+    ax.plot(x, y, *args, **kwargs)
+
+
 def weighing_plot(request, subject_id=None):
     if not request.user.is_authenticated():
         return HttpResponse('')
+    if subject_id in (None, 'None'):
+        return HttpResponse('')
 
+    # Import matplotlib.
     try:
+        import numpy as np
         import matplotlib
     except ImportError:
-        return HttpResponse('Please install matplotlib.')
-
+        return HttpResponse('Please install numpy and matplotlib.')
     matplotlib.use('Agg')
     import matplotlib.pyplot as plt
     from matplotlib.dates import DayLocator, DateFormatter
-    # plt.style.use('seaborn')
-
-    if subject_id in (None, 'None'):
-        return HttpResponse('')
-    subj = Subject.objects.get(pk=subject_id)
-    weighins = Weighing.objects.filter(
-        subject_id=subj.id).order_by('date_time')
-
     f, ax = plt.subplots(1, 1, figsize=(8, 2))
+
+    # Get data.
+    subj = Subject.objects.get(pk=subject_id)
+    weighings = Weighing.objects.filter(subject=subj).order_by('date_time')
+    eweighings = [subj.expected_weighing(subj.to_weeks(w.date_time))
+                  for w in weighings]
+
+    # Axes.
     ax.xaxis.set_major_locator(DayLocator(interval=14))
     ax.xaxis.set_major_formatter(DateFormatter('%d/%m/%y'))
 
-    if weighins:
-        x, y = zip(*((w.date_time, w.weight) for w in weighins))
-        ax.plot(x, y)
+    # Plots.
+    _plot_weighings(ax, weighings)
+    _plot_weighings(ax, eweighings, coeff=.7)
+    _plot_weighings(ax, eweighings, coeff=.8)
+    y0, y1 = ax.get_ylim()
+    x = [w.weight for w in weighings]
+    n = len(x)
+    where = [w.weight < ew.weight * .8 for w, ew in zip(weighings, eweighings)]
+    ax.fill_between(x, y0 * np.ones(n), y1 * np.ones(n), where=where, interpolate=True)
+
+    # Params.
     ax.set_title("Weighings for %s" % subj.nickname)
     plt.tight_layout()
     plt.grid('on')
 
+    # Return the PNG.
     buf = io.BytesIO()
     f.savefig(buf, format='png')
     buf.seek(0)
