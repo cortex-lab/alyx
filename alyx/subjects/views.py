82b3ad0de234c3791a4cccb91ae588e83a139dee
Max Hunter <nippoo@nippoo.net>
2017-03-28 17:02:32 +0100

    Added stock BooleanFilter

--- alyx/subjects/views.py
+++ alyx/subjects/views.py
@@ -9,6 +9,13 @@ from django_filters.rest_framework import FilterSet
 class SubjectFilter(FilterSet):
     alive = django_filters.BooleanFilter(name='death_date', lookup_expr='isnull')
     responsible_user = django_filters.CharFilter(name='responsible_user__username')
+    stock = django_filters.BooleanFilter(name='responsible_user', method='filter_stock')
+
+    def filter_stock(self, queryset, name, value):
+        if value == True:
+            return queryset.filter(responsible_user__id=5)
+        else:
+            return queryset.exclude(responsible_user__id=5)
 
     class Meta:
         model = Subject
