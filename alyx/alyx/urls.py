0d729d0ba6f40a254a604c75a8cd44cc9c2bfe35
Max Hunter <nippoo@nippoo.net>
2017-03-29 17:15:13 +0100

    Added water restricted endpoint URL to APIRouter

--- alyx/alyx/urls.py
+++ alyx/alyx/urls.py
@@ -90,7 +90,7 @@ urlpatterns = [
     url(r'^users$', user_list, name='user-list'),
     url(r'^users/(?P<username>[-_\w].+) $', user_detail, name='user-detail'),
 
-    url(r'^water-restricted-subjects$', subjects_views.WaterRestrictedSubjectList.as_view(), name="water-restricted-subjects-list"),
+    url(r'^water-restricted-subjects$', subjects_views.WaterRestrictedSubjectList.as_view(), name="water-restricted-subject-list"),
 
     url(r'^admin/', include(admin.site.urls)),
 
