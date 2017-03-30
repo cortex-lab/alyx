c7cf85d18a71dc717ba0422d45f8386359ee39e6
Max Hunter <nippoo@nippoo.net>
2017-03-30 18:43:40 +0100

    Added --remove-pickle option to delete dumped_google_sheets.pkl

--- alyx/misc/management/commands/import_data.py
+++ alyx/misc/management/commands/import_data.py
@@ -14,8 +14,20 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         super(Command, self).add_arguments(parser)
+        parser.add_argument(
+        '-R', '--remove-pickle', action='store_true',
+        dest='remove_pickle', default=False,
+        help='Removes and redownloads dumped_google_sheets.pkl')
 
     def handle(self, *args, **options):
+
+        if options.get('remove_pickle'):
+            try:
+                os.remove(op.join(op.dirname( __file__ ), '../../../../data/', 'dumped_google_sheets.pkl'))
+                self.stdout.write('Removed dumped_google_sheets.pkl')
+            except FileNotFoundError:
+                self.stdout.write('Could not remove dumped_google_sheets.pkl: file does not exist')
+
         import_data()
         call_command('migrate')
 
