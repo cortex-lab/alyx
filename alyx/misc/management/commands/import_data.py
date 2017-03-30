b8ed277862f118726d8485c0c76997004d740a3f
Max Hunter <nippoo@nippoo.net>
2017-03-30 15:51:56 +0100

    Refactored update_zygosities into a separate ManagementCommand

--- alyx/misc/management/commands/import_data.py
+++ alyx/misc/management/commands/import_data.py
@@ -1,12 +1,8 @@
-"""
-adapted from http://www.djangosnippets.org/snippets/828/ by dnordberg
-"""
 import logging
 
 import django
 from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
-import psycopg2 as Database
 from ..importer import import_data
 from django.core.management import call_command
 import os
@@ -29,14 +25,4 @@ class Command(BaseCommand):
                     fullpath = op.join(op.dirname( __file__ ), '../../../../', 'data/json', file)
                     call_command('loaddata', fullpath, verbosity=3, interactive=False)
 
-
-        from subjects.models import ZygosityFinder, Subject
-
-        zf = ZygosityFinder()
-
-        print("Updating the zygosities...")
-        for subject in Subject.objects.all():
-            zf.genotype_from_litter(subject)
-            zf.update_subject(subject)
-
-        self.stdout.write(self.style.SUCCESS('Import successful!'))
+        self.stdout.write(self.style.SUCCESS('Loaded all JSON files from data folder!'))
