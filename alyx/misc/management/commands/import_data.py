fb4204489957ebc22177e1be989cca40cc9b3d5b
Max Hunter <nippoo@nippoo.net>
2017-04-05 19:44:07 +0100

    Added command-line flag to pass data_dir rather than hardcoding

--- alyx/misc/management/commands/import_data.py
+++ alyx/misc/management/commands/import_data.py
@@ -14,6 +14,9 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         super(Command, self).add_arguments(parser)
+
+        parser.add_argument('data_dir', nargs=1, type=str)
+
         parser.add_argument(
         '-R', '--remove-pickle', action='store_true',
         dest='remove_pickle', default=False,
@@ -21,20 +24,32 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
+        data_dir = options.get('data_dir')[0]
+        if not os.path.isdir(data_dir):
+            self.stdout.write('Error: %s is not a directory' % data_dir)
+            return
+
         if options.get('remove_pickle'):
             try:
-                os.remove(op.join(op.dirname( __file__ ), '../../../../data/', 'dumped_google_sheets.pkl'))
+                os.remove(op.join(data_dir, 'dumped_google_sheets.pkl'))
                 self.stdout.write('Removed dumped_google_sheets.pkl')
             except FileNotFoundError:
                 self.stdout.write('Could not remove dumped_google_sheets.pkl: file does not exist')
+                return
 
         import_data()
         call_command('migrate')
 
-        for root, dirs, files in os.walk('../data/json'):
+        json_dir = op.join(data_dir, 'json')
+
+        if not os.path.isdir(data_dir):
+            self.stdout.write('Error: %s does not exist: it should contain .json files' % json_dir)
+            return
+
+        for root, dirs, files in os.walk(json_dir):
             for file in files:
                 if file.endswith('.json'):
-                    fullpath = op.join(op.dirname( __file__ ), '../../../../', 'data/json', file)
+                    fullpath = op.join(json_dir, file)
                     call_command('loaddata', fullpath, verbosity=3, interactive=False)
 
-        self.stdout.write(self.style.SUCCESS('Loaded all JSON files from data folder!'))
+        self.stdout.write(self.style.SUCCESS('Loaded all JSON files from %s' % json_dir))
