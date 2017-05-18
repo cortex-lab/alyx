18ba93e4d35e0497bc9da19e86483870129f1933
Cyrille Rossant <cyrille.rossant@gmail.com>
2017-05-18 16:16:47 +0200

    Fix crash with REST API due to missing template

--- alyx/alyx/settings.py
+++ alyx/alyx/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'polymorphic',
+    'django_filters',
     'rest_framework',
     'rest_framework.authtoken',
     'equipment',
