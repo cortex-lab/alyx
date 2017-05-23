43224616f7432aca3e76bc006f3731c0d83c6d01
Cyrille Rossant <cyrille.rossant@gmail.com>
2017-05-23 11:10:53 +0200

    Add subject API test

--- alyx/subjects/tests_rest.py
+++ alyx/subjects/tests_rest.py
@@ -2,8 +2,6 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from alyx.base import BaseTests
 
-from rest_framework import status
-
 
 class APISubjectsTests(BaseTests):
     def setUp(self):
@@ -12,6 +10,14 @@ class APISubjectsTests(BaseTests):
 
     def test_list_subjects(self):
         url = reverse('subject-list')
+        response = self.client.get(url)
+        self.ar(response)
+        d = response.data
+        assert len(d) > 200
+        assert set(('nickname', 'id', 'responsible_user', 'death_date')) <= set(d[0])
+
+    def test_list_alive_subjects(self):
+        url = reverse('subject-list') + '?alive=True&stock=True'
         response = self.client.get(url)
         self.ar(response)
         d = response.data
