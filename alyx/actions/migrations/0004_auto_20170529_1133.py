d891cb6588b68021353cd38d24137d60bfb5839d
Cyrille Rossant <cyrille.rossant@gmail.com>
2017-06-19 10:07:29 +0200

    WIP

--- alyx/actions/migrations/0004_auto_20170529_1133.py
+++ alyx/actions/migrations/0004_auto_20170529_1133.py
@@ -13,42 +13,74 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterField(
+        migrations.RemoveField(
+            model_name='otheraction',
+            name='users',
+        ),
+        migrations.RemoveField(
+            model_name='session',
+            name='users',
+        ),
+        migrations.RemoveField(
+            model_name='surgery',
+            name='location',
+        ),
+        migrations.RemoveField(
+            model_name='surgery',
+            name='users',
+        ),
+        migrations.RemoveField(
+            model_name='virusinjection',
+            name='users',
+        ),
+        migrations.RemoveField(
+            model_name='wateradministration',
+            name='user',
+        ),
+        migrations.RemoveField(
+            model_name='waterrestriction',
+            name='users',
+        ),
+        migrations.RemoveField(
+            model_name='weighing',
+            name='user',
+        ),
+        migrations.AddField(
             model_name='otheraction',
             name='users',
             field=models.ManyToManyField(blank=True, help_text='The user(s) involved in this action', to='misc.OrderedUser'),
         ),
-        migrations.AlterField(
+        migrations.AddField(
             model_name='session',
             name='users',
             field=models.ManyToManyField(blank=True, help_text='The user(s) involved in this action', to='misc.OrderedUser'),
         ),
-        migrations.AlterField(
+        migrations.AddField(
             model_name='surgery',
             name='location',
             field=models.ForeignKey(blank=True, help_text='The physical location at which the surgery was performed', null=True, on_delete=django.db.models.deletion.CASCADE, to='equipment.LabLocation'),
         ),
-        migrations.AlterField(
+        migrations.AddField(
             model_name='surgery',
             name='users',
             field=models.ManyToManyField(blank=True, help_text='The user(s) involved in this action', to='misc.OrderedUser'),
         ),
-        migrations.AlterField(
+        migrations.AddField(
             model_name='virusinjection',
             name='users',
             field=models.ManyToManyField(blank=True, help_text='The user(s) involved in this action', to='misc.OrderedUser'),
         ),
-        migrations.AlterField(
+        migrations.AddField(
             model_name='wateradministration',
             name='user',
             field=models.ForeignKey(blank=True, help_text='The user who administered water', null=True, on_delete=django.db.models.deletion.CASCADE, to='misc.OrderedUser'),
         ),
-        migrations.AlterField(
+        migrations.AddField(
             model_name='waterrestriction',
             name='users',
             field=models.ManyToManyField(blank=True, help_text='The user(s) involved in this action', to='misc.OrderedUser'),
         ),
-        migrations.AlterField(
+        migrations.AddField(
             model_name='weighing',
             name='user',
             field=models.ForeignKey(blank=True, help_text='The user who weighed the subject', null=True, on_delete=django.db.models.deletion.CASCADE, to='misc.OrderedUser'),
