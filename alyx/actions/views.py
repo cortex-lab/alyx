28ab69516b68c41e8b8613926dbbae9bd444bf81
rossant <cyrille.rossant@gmail.com>
2017-12-11 11:36:53 +0100

    Fix in water-history: the last date was being discarded

--- alyx/actions/views.py
+++ alyx/actions/views.py
@@ -28,7 +28,7 @@ from .serializers import (SessionListSerializer,
 
 
 def date_range(start_date, end_date):
-    for n in range(int((end_date - start_date).days)):
+    for n in range(int((end_date - start_date).days) + 1):
         yield (start_date + timedelta(n)).date()
 
 
