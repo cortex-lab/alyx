4f2943205728adba45c38263413ce23566433b9f
Cyrille Rossant <rossant@users.noreply.github.com>
2018-04-19 18:02:09 +0200

    Disable time zone support; times are saved in local timezone in the DB

--- alyx/alyx/settings.py
+++ alyx/alyx/settings.py
@@ -199,7 +199,7 @@ TIME_ZONE = 'GB'
 
 USE_I18N = False
 USE_L10N = False
-USE_TZ = True
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
