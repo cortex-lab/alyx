bfa8ab329a5db01701924c3e0d32b9084cb9c66e
Olivier Winter <olivier.winter@research.fchampalimaud.org>
2018-09-07 10:38:45 +0100

    Globus transfer bugfix
    Maximum length of label field is 128 chars. Prevents the transfer from happening.

--- alyx/data/transfers.py
+++ alyx/data/transfers.py
@@ -118,7 +118,7 @@ def start_globus_transfer(source_file_id, destination_file_id, dry_run=False):
     tc = globus_transfer_client()
     tdata = globus_sdk.TransferData(
         tc, source_id, destination_id, verify_checksum=True, sync_level='checksum',
-        label=label,
+        label=label[0: min(len(label), 128)],
     )
     tdata.add_item(source_path, destination_path)
 
