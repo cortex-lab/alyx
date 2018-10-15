99b65f765e560aa29f7af538c32326d8d0d0c186
Cyrille Rossant <rossant@users.noreply.github.com>
2018-10-15 10:23:24 +0200

    Remove scripts

--- alyx/backup.sh
+++
@@ -1,1 +0,0 @@
-pg_dump -cOx -U cyrille -h localhost alyx -f alyx_full.local.sql
--- alyx/updatedb.sh
+++
@@ -1,4 +0,0 @@
-scp cone:/var/www/alyx/alyx-backups/$(date +%Y-%m-%d)/alyx_full.sql.gz .
-./manage.py reset_db
-gunzip alyx_full.sql.gz
-psql -h localhost -U cyrille -d alyx -f alyx_full.sql
