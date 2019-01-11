becab9e238eddaf6958bf92024c76320042ad01a
Olivier Winter <oliche@gmail.com>
2019-01-11 08:57:23 +0000

    session date filter on start time (case if session ran over midnight)

--- alyx/actions/views.py
+++ alyx/actions/views.py
@@ -84,8 +84,8 @@ class SessionFilter(FilterSet):
     def filter_date_range(self, queryset, name, value):
         drange = value.split(',')
         queryset = queryset.filter(
-            Q(start_time__date__gte=drange[0]) | Q(end_time__date__gte=drange[0]),
-            Q(start_time__date__lte=drange[1]) | Q(end_time__date__lte=drange[1]),
+            Q(start_time__date__gte=drange[0]),
+            Q(start_time__date__lte=drange[1]),
         )
         return queryset
 
