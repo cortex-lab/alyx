1da2b7c6df33eb30ee3ba4d297e560b75fdd6fe7
Olivier Winter <oliche@gmail.com>
2019-01-11 09:01:38 +0000

    Set debug to False by default - flakify

--- alyx/actions/water_control.py
+++ alyx/actions/water_control.py
@@ -452,7 +452,8 @@ def water_control(subject):
         if len(Lab.objects.all()) == 1:
             lab = Lab.objects.first()
     if lab is None:
-        logger.warning("Subject %s has no lab, no reference weight percentages considered.", subject)
+        logger.warning("Subject %s has no lab, no reference weight percentages considered.",
+                       subject)
         rw_pct = zw_pct = 0
     else:
         rw_pct = lab.reference_weight_pct
