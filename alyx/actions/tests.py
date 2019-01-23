cc0014e1b0470842bb383ec9f892c58764f0578b
Cyrille Rossant <rossant@users.noreply.github.com>
2019-01-23 10:18:54 +0100

    Fix water control tests

--- alyx/actions/tests.py
+++ alyx/actions/tests.py
@@ -12,20 +12,17 @@ from actions.models import (
 from actions.notifications import check_water_administration
 from misc.models import LabMember, LabMembership
 from subjects.models import Subject
-from misc.models import Lab
 
 
 class WaterControlTests(TestCase):
     def setUp(self):
         base.DISABLE_MAIL = True
-        # create some water types
         wtypes = ['Water', 'Hydrogel', 'CA 5% Hydrogel', 'CA 5%', 'Sucrose 10%']
         for wt in wtypes:
             WaterType.objects.create(name=wt)
         # create a subject
-        lab = Lab.objects.create(name='lab')
-        self.sub = Subject.objects.create(
-            nickname='bigboy', birth_date='2018-09-01', lab=lab)
+        sub = Subject.objects.create(nickname='bigboy', birth_date='2018-09-01')
+        self.sub = Subject.objects.get(pk=sub.pk)
         # 50 days of loosing weight and getting 0.98 mL water
         self.start_date = datetime.datetime(year=2018, month=10, day=1)
         self.rwind = 5  # water restriction will start on the fifth day
@@ -40,17 +37,10 @@ class WaterControlTests(TestCase):
         # first test assert that water administrations previously created have the correct default
         wa = WaterAdministration.objects.filter(subject=self.sub)
         self.assertTrue(wa.values_list('water_type__name').distinct()[0][0] == 'Water')
-
-    def tearDown(self):
-        base.DISABLE_MAIL = False
-
-    def test_00_create_first_water_restriction(self):
-
         # create labs with different weight measurement techniques
         Lab.objects.create(name='zscore', reference_weight_pct=0, zscore_weight_pct=0.85)
         Lab.objects.create(name='rweigh', reference_weight_pct=0.85, zscore_weight_pct=0)
         Lab.objects.create(name='mixed', reference_weight_pct=0.425, zscore_weight_pct=0.425)
-
         # Create an initial Water Restriction
         start_wr = self.start_date + datetime.timedelta(days=self.rwind)
         water_type = WaterType.objects.get(name='CA 5% Hydrogel')
@@ -63,6 +53,9 @@ class WaterControlTests(TestCase):
             date_time=datetime.datetime.now())
         self.assertEqual(water_type, wa.water_type)
 
+    def tearDown(self):
+        base.DISABLE_MAIL = False
+
     def test_water_administration_expected(self):
         wc = self.sub.water_control
         wa = WaterAdministration.objects.filter(subject=self.sub)
@@ -70,7 +63,31 @@ class WaterControlTests(TestCase):
         self.assertTrue(wa[0].expected() == wc.expected_water(date=wa[0].date_time.date()))
         self.assertTrue(wa[40].expected() == wc.expected_water(date=wa[40].date_time.date()))
 
-        
+    def test_water_control_thresholds(self):
+        # test computation on reference weight lab alone
+        self.sub.lab = Lab.objects.get(reference_weight_pct=0.85)
+        self.sub.save()
+        wc = self.sub.water_control
+        wc.expected_weight()
+        self.assertAlmostEqual(self.wei[self.rwind], wc.reference_weight())
+        self.assertAlmostEqual(self.wei[self.rwind], wc.expected_weight())
+        # test computation on zscore weight lab alone
+        self.sub.lab = Lab.objects.get(reference_weight_pct=0)
+        self.sub.save()
+        wc = self.sub.water_control
+        wc.expected_weight()
+        self.assertAlmostEqual(self.wei[self.rwind], wc.reference_weight())
+        zscore = wc.zscore_weight()
+        # test computation on mixed lab
+        self.sub.lab = Lab.objects.get(reference_weight_pct=0.425)
+        self.sub.save()
+        wc = self.sub.water_control
+        self.assertAlmostEqual(self.wei[self.rwind], wc.reference_weight())
+        self.assertAlmostEqual(wc.expected_weight(), (wc.reference_weight() + zscore) / 2)
+        # test that the thresholds are all above 70%
+        self.assertTrue(all([thrsh[0] > 0.4 for thrsh in wc.thresholds]))
+
+
 class NotificationTests(TestCase):
     def setUp(self):
         base.DISABLE_MAIL = True
@@ -196,28 +213,3 @@ class NotificationTests(TestCase):
         nr.subjects_scope = 'none'
         nr.save()
         _assert_users([self.user2], [self.user2])
-
-    def test_water_control_thresholds(self):
-        # test computation on reference weight lab alone
-        self.sub.lab = Lab.objects.get(reference_weight_pct=0.85)
-        self.sub.save()
-        wc = self.sub.water_control
-        wc.expected_weight()
-        self.assertAlmostEqual(self.wei[self.rwind], wc.reference_weight())
-        self.assertAlmostEqual(self.wei[self.rwind], wc.expected_weight())
-        # test computation on zscore weight lab alone
-        self.sub.lab = Lab.objects.get(reference_weight_pct=0)
-        self.sub.save()
-        wc = self.sub.water_control
-        wc.expected_weight()
-        self.assertAlmostEqual(self.wei[self.rwind], wc.reference_weight())
-        zscore = wc.zscore_weight()
-        # test computation on mixed lab
-        self.sub.lab = Lab.objects.get(reference_weight_pct=0.425)
-        self.sub.save()
-        wc = self.sub.water_control
-        self.assertAlmostEqual(self.wei[self.rwind], wc.reference_weight())
-        self.assertAlmostEqual(wc.expected_weight(), (wc.reference_weight() + zscore) / 2)
-        # test that the thresholds are all above 70%
-        self.assertTrue(all([thrsh[0] > 0.4 for thrsh in wc.thresholds]))
-
