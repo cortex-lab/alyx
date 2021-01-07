f3e2f75a73bff4e97aa00bea48cb4b34f9cfe1ba
Gaelle Chapuis <gaelle.chapuis.ibl@gmail.com>
2021-01-07 13:56:28 +0100

    WIP reports

--- alyx/reports/views.py
+++ alyx/reports/views.py
@@ -2,19 +2,21 @@ from django.http import HttpResponse
 
 from django.template import loader
 
-from experiments.models import TrajectoryEstimate
+from experiments.models import TrajectoryEstimate, ProbeInsertion
 from django.db.models import Count, Q
 from misc.models import Lab
 
 
-import datetime
-
 def current_datetime(request):
-    # now = datetime.datetime.now()
     template = loader.get_template('reports/simple.html')
-    # context = {'now': now}
 
-    te = TrajectoryEstimate.objects.filter(
+    # BRAINWIDE INSERTIONS
+    ins = ProbeInsertion.objects.filter(
+        session__subject__projects__name='ibl_neuropixel_brainwide_01',
+        session__qc__lt=50)
+
+    # REP SITE
+    traj = TrajectoryEstimate.objects.filter(
         provenance=10, x=-2243, y=-2000,
         probe_insertion__session__subject__projects__name='ibl_neuropixel_brainwide_01',
         probe_insertion__session__qc__lt=50)
@@ -22,12 +24,14 @@ def current_datetime(request):
     labs = Lab.objects.all()
     labs = labs.annotate(
         nrep=Count('subject__actions_sessions__probe_insertion__trajectory_estimate',
-        filter=Q(subject__actions_sessions__probe_insertion__trajectory_estimate__in=te)))
+        filter=Q(subject__actions_sessions__probe_insertion__trajectory_estimate__in=traj)))
     # print(l.name, l.nrep)
 
     context = {
-        "total": te.count(),
+        "total": traj.count(),
         "labs": labs,
+        "traj": traj,
+        "ins": ins
     }
 
     return HttpResponse(template.render(context, request))
