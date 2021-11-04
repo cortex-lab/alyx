9a918c769554cb345bec9794152eb9f90ddaa82d
juhuntenburg <ju.huntenburg@gmail.com>
2021-11-04 13:05:45 +0000

    Revert change in manage.py

--- alyx/manage.py
+++ alyx/manage.py
@@ -3,10 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    if 'GITHUB_ACTIONS' in os.environ:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alyx.settings_template")
-    else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alyx.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alyx.settings")
 
     from django.core.management import execute_from_command_line
 
