538ec6bb11b30ce199bb5fa1bc02f06a39d46d63
olivier <olivier.winter@hotmail.fr>
2022-06-23 11:11:24 +0100

    projects changes: views

--- alyx/experiments/views.py
+++ alyx/experiments/views.py
@@ -145,7 +145,7 @@ Trajectory Estimates objects REST filters and views
 class TrajectoryEstimateFilter(BaseFilterSet):
     provenance = CharFilter(method='enum_field_filter')
     subject = CharFilter('probe_insertion__session__subject__nickname')
-    project = CharFilter('probe_insertion__session__project__name')
+    project = CharFilter('probe_insertion__session__projects__name')
     date = CharFilter('probe_insertion__session__start_time__date')
     experiment_number = CharFilter('probe_insertion__session__number')
     session = UUIDFilter('probe_insertion__session__id')
