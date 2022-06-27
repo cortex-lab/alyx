5fdbc13b1919c1bf76827f49a1f7458c45e10f13
Miles Wells <k1o0@3tk.co>
2022-06-27 13:31:45 +0300

    Tests & migrations

--- alyx/data/tests.py
+++ alyx/data/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from data.models import Dataset
+from django.db.utils import IntegrityError
+from data.models import Dataset, DatasetType
+from data.transfers import get_dataset_type
 
 
 class TestModel(TestCase):
@@ -8,3 +10,26 @@ class TestModel(TestCase):
         assert dset.is_online is False
         assert dset.is_public is False
         assert dset.is_protected is False
+
+
+class TestDatasetTypeModel(TestCase):
+    def test_model_methods(self):
+        dtype, _ = DatasetType.objects.get_or_create(
+            name='obj.attr', description='thing', filename_pattern=None)
+        dtype2, _ = DatasetType.objects.get_or_create(
+            name='foo.bar', description='foo bar', filename_pattern='*foo.b?r*')
+        dtype3, _ = DatasetType.objects.get_or_create(
+            name='bar.baz', description='.', filename_pattern=None)
+        with self.assertRaises(IntegrityError):
+            DatasetType.objects.get_or_create(name='objFoo.bar', filename_pattern='*foo.bar*')
+        with self.assertRaises(IntegrityError):
+            DatasetType.objects.get_or_create(name='obj.attr', filename_pattern='-')
+        filename_typename = (
+            ('foo.bar.npy', 'foo.bar'),
+            ('foo.bir.npy', 'foo.bar'),
+            ('_ns_foo.bar_clock.extra.npy', 'foo.bar'),
+            ('bar.baz.ext', 'bar.baz')
+        )
+        for filename, dataname in filename_typename:
+            with self.subTest(filename=filename):
+                self.assertEqual(get_dataset_type(filename).name, dataname)
