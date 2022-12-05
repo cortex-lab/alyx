fdabcf9a812b09c0c6f25b973d7f5dc6058e34e2
mayofaulkner <mayo.faulkner@ucl.ac.uk>
2022-12-05 17:09:17 +0000

    flake!

--- alyx/experiments/tests_rest.py
+++ alyx/experiments/tests_rest.py
@@ -267,7 +267,7 @@ class APISubjectsTests(BaseTests):
 
         ci = self.ar(self.post(reverse('chronicinsertion-list'), chronic_dict), 201)
 
-        # create the probe insertion with a related chronic insertion, 
+        # create the probe insertion with a related chronic insertion,
         # first without the serial number and make sure it errors
         probe_dict = {'session': str(self.session.id),
                       'name': 'probe00',
