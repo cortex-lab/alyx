b7cefb8a5e63fe228a3b4bce2ce89c23d006290e
Miles Wells <k1o0@5tk.co>
2026-01-08 14:51:07 +0200

    Moved prune_cortexlab.py to iblalyx

--- scripts/sync_ucl/ucl_post_json_import.py
+++
@@ -1,9 +0,0 @@
-from misc.models import Lab
-from experiments.models import ProbeInsertion
-(Lab.objects
- .filter(name='cortexlab')
- .update(json=Lab.objects.using('cortexlab').get(name='cortexlab').json)
- )
-
-for pi in ProbeInsertion.objects.filter(session__lab__name='cortexlab'):
-    pi.save()
